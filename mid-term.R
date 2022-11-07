7f15a4a3377a00c74ffcdfad76a0816740138585
SpriteLiu <111390047+SpriteLiu@users.noreply.github.com>
2022-11-06 23:12:59 -0500

    Add files via upload

---
+++ mid-term.R
@@ -0,0 +1,90 @@
+myName <- "Hsueh-Pin Liu"
+
+library(tidyverse)
+library(magrittr)
+library(readxl)
+
+strawb <- read_xlsx("Desktop/MA 615 Homework/untitled folder/strawberries-2022oct30-a.xlsx")
+
+cnames <- colnames(strawb)
+x <- 1:dim(strawb)[2]
+
+unique(strawb[1])
+unique(strawb[2])
+unique(strawb[3])
+
+T <- NULL
+for(i in x){T <- c(T, dim(unique(strawb[i]))[1])}
+drop_cols <- cnames[which(T == 1)]
+strawb %<>% dplyr::select(!all_of(drop_cols))
+strawb %<>% arrange(Year, State)
+
+colnames(strawb)
+temp1 <- strawb %>% dplyr::select(`Data Item`) %>% 
+         distinct()
+strawb2 <- strawb %>% separate(col=`Data Item`,
+                into = c("Strawberries", "items", "units"),
+                sep = ",",
+                fill = "right")
+strawb3 <- strawb %>% separate(col=`Data Item`,
+            into = c("Strawberries", "type", "items", "units"),
+                               sep = ",",
+                               fill = "right")
+rm(strawb2, strawb3)
+strawb %<>% separate(col=`Data Item`,
+                    into = c("Strawberries", "type", "items", "units"),
+                    sep = ",",
+                    fill = "right")
+
+#1
+#285CWT=28500LB
+
+#2
+s1 <- filter(strawb, State == 'CALIFORNIA' & 
+               Year == 2016 & 
+               Domain == 'ORGANIC STATUS')
+c1 <- as.numeric(s1$Value)
+t.test(c1)
+#(-39779720,194947329)
+
+#3
+s2 <- filter(strawb, State == 'CALIFORNIA' & 
+               Year == 2016 & 
+               Domain != 'ORGANIC STATUS')
+s2 <- filter(s2, Value != "(NA)" & 
+         Value != "(D)" & 
+         Domain != "TOTAL")
+c2 <- as.numeric(s2$Value)
+t.test(c2)
+#(-10378.79,199946.13)
+
+#4
+unique(strawb[10])
+s3 <- filter(strawb, Domain != 'ORGANIC STATUS' & 
+                     Domain != 'TOTAL')
+a <- grep("TOTAL",
+     chemical$`Domain Category`,
+     ignore.case = T)
+unique(s3[11])
+#175-36=139
+
+#5
+s4 <- filter(strawb, State == 'FLORIDA' & 
+               Domain != 'ORGANIC STATUS' & 
+               Domain != 'TOTAL')
+s5 <- filter(strawb, State == 'CALIFORNIA' & 
+         Domain != 'ORGANIC STATUS' & 
+         Domain != 'TOTAL')
+a <- grep("TOTAL",
+     s4$`Domain Category`,
+     ignore.case = T)
+unique(s4[11])
+#(142-16)-(119-16)=23
+
+
+
+
+
+
+
+
